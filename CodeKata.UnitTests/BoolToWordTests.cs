3c9796e1fe34ffd58345738216121303720d808f
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-07-25 17:51:38 +0200

    Create a new project with tests

---
+++ CodeKata.UnitTests/BoolToWordTests.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeKata.UnitTests
+{
+    [TestFixture]
+    public class BoolToWordTests
+    {
+
+        [Test]
+        public void boolToWordReturned1()
+        {
+            Assert.AreEqual("Yes", BoolToWord.ConvertBoolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+
+        [Test]
+        public void boolToWordReturned2()
+        {
+            Assert.AreEqual("No", BoolToWord.ConvertBoolToWord(false));
+            Console.WriteLine("Expected No");
+        }
+
+        [Test]
+        public void boolToWordReturned3()
+        {
+            Assert.AreEqual("Yes", BoolToWord.ConvertBoolToWord(true));
+            Console.WriteLine("Expected Yes");
+        }
+    }
+}
---
+++ CodeKata/BoolToWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public static class BoolToWord
+    {
+        public static string ConvertBoolToWord(bool word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
