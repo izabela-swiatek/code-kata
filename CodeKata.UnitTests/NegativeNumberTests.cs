f4f06eaca1ef4c7b51ccfefaa53bee82e7cfd274
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-08-02 23:28:43 +0200

    Add method and test to NegativeNumber

---
+++ CodeKata.UnitTests/NegativeNumberTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    public class NegativeNumberTests
+    {
+
+        [Test]
+        public void MakeNegative_Positive_ReturnsNegative()
+        {
+            Assert.AreEqual(-42, NegativeNumber.MakeNegative(42));
+        }
+    }
+}
