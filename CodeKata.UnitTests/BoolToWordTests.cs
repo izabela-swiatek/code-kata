87a53620fc16c0cef6e79ccebfed0edafd41b3e1
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-08-03 22:42:58 +0200

    add new BoolToWorld Test

--- CodeKata.UnitTests/BoolToWordTests.cs
+++ CodeKata.UnitTests/BoolToWordTests.cs
@@ -3,6 +3,26 @@
     [TestFixture]
     public class BoolToWordTests
     {
+        private static object[] testCases =
+        {
+            new object[]{ true, "Yes" },
+            new object[]{ false, "No" }
+        };
+
+        [TestCaseSource("testCases")]
+        public void ConvertBoolToWord_PredicateFromTestCaseSource_ReturnsYesForTrueNoForFalse(bool predicate, string expected)
+        {
+            Assert.AreEqual(expected, BoolToWord.ConvertBoolToWord(predicate));
+            Console.WriteLine($"Expected {expected}");
+        }
+
+        [TestCase(true, "Yes")]
+        [TestCase(false, "No")]
+        public void ConvertBoolToWord_PredicateFromTestCase_ReturnsYesForTrueNoForFalse(bool predicate, string expected)
+        {
+            Assert.AreEqual(expected, BoolToWord.ConvertBoolToWord(predicate));
+            Console.WriteLine($"Expected {expected}");
+        }
 
         [Test]
         public void ConvertBoolToWord_True_ReturnsYes()
@@ -17,7 +37,5 @@
             Assert.AreEqual("No", BoolToWord.ConvertBoolToWord(false));
             Console.WriteLine("Expected No");
         }
-
-        
     }
 }
