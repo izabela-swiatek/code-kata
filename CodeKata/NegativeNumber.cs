0a18576d76d0fd5fec68d651fe05f32d50155c33
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-08-06 21:35:06 +0200

    Given a number make it negative (#4)
    * Add method and test to NegativeNumber
    
    * remove extra line and semicolon

---
+++ CodeKata/NegativeNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class NegativeNumber
+    {
+        public static int MakeNegative(int number)
+        {
+            return number < 0 ? number : number * -1;
+        }
+    }
+}
