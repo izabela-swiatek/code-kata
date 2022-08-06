dbc6b4cbd690d1256b6d3eadebc91b120ac768d5
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-08-06 21:33:39 +0200

    Create SumArray method and tests (#6)

---
+++ CodeKata.UnitTests/TakeArrayReturnSumTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    [TestFixture]
+    public class TakeArrayReturnSumTests
+    {
+        private static object[] testCases = new object[]
+        {
+            new object[] {new double[] {1, 5.2, 4, 0, -1}, 9.2},
+            new object[] {new double[] {}, 0},
+        };
+
+        [Test, TestCaseSource("testCases")]
+        public void Test(double[] array, double expected) => Assert.AreEqual(expected, TakeArrayReturnSum.SumArray(array));
+    }
+}
---
+++ CodeKata/TakeArrayReturnSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class TakeArrayReturnSum
+    {
+        public static double SumArray(double[] array)
+        {
+            double output = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                output += array[i];
+            }
+
+            return output;
+        }
+    }
+}
