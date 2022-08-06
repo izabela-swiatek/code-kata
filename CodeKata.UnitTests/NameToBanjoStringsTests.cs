e8dbd8f47cfc2d59982d8ee25dd4d8e28fc224cd
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-08-06 22:54:34 +0200

    Write a method AreYouPlayingBanjo and Tests

---
+++ CodeKata.UnitTests/NameToBanjoStringsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    public class NameToBanjoStringsTests
+    {
+        [Test]
+        public void AreYouPlayingBanjo_RStartString_ReturnsPlays()
+        {
+            Assert.AreEqual("Ricardo plays banjo", NameToBanjoStrings.AreYouPlayingBanjo("Ricardo"));
+        }
+
+        [Test]
+        public void AreYouPlayingBanjo_rStartString_ReturnsPlays()
+        {
+            Assert.AreEqual("reksin plays banjo", NameToBanjoStrings.AreYouPlayingBanjo("reksin"));
+        }
+
+        [Test]
+        public void AreYouPlayingBanjo_NoneRStartString_ReturnsNotPlay()
+        {
+            Assert.AreEqual("Arjuna does not play banjo", NameToBanjoStrings.AreYouPlayingBanjo("Arjuna"));
+        }
+
+        [Test]
+        public void AreYouPlayingBanjo_NonerStartString_ReturnsNotPlay()
+        {
+            Assert.AreEqual("melissa does not play banjo", NameToBanjoStrings.AreYouPlayingBanjo("melissa"));
+        }
+
+    }
+}
