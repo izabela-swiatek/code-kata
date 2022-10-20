8925ea4ae24a10a3926763253eb5cff7cc8633d3
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-20 09:24:11 +0200

    Write a method AreYouPlayingBanjo and Tests (#9)
    * Write a method AreYouPlayingBanjo and Tests
    
    * Make the method case insensitive

---
+++ CodeKata/NameToBanjoStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class NameToBanjoStrings
+    {
+        public static string AreYouPlayingBanjo(string name)
+        {
+            if (char.ToLower(name[0]) == 'r')
+            {
+                return $"{name} plays banjo";
+            }
+            else 
+            {
+                return $"{name} does not play banjo";
+            }
+        }
+    }
+}
