82ca56d7c8c8e803b469465a965095be887a782f
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-10-20 11:39:28 +0200

    Write ChangeLight method and tests

---
+++ CodeKata.UnitTests/TrafficLightsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class TrafficLightsTests
+    {
+        [TestCase("green", "yellow")]
+        [TestCase("yellow", "red")]
+        [TestCase("red", "green")]
+        public void TrafficLIghts_PreviousLight_ReturnsNextLight(string previousLight, string nextLight)
+        {
+            Assert.That(TrafficLights.UpdateLight(previousLight), Is.EqualTo(nextLight));
+        }
+    }
+}
