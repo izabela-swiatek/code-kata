a4958e1953c13eb3dd1689222b5d7086b60444b4
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-20 11:58:11 +0200

    Write ChangeLight method and tests (#11)

---
+++ CodeKata/TrafficLights.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class TrafficLights
+    {
+        public static string UpdateLight(string current)
+        {
+            switch (current)
+            {
+                case "green":
+                    return "yellow";
+                case "yellow":
+                    return "red";
+                case "red":
+                    return "green";
+                default:
+                    return "";
+            }
+        }
+    }
+}
