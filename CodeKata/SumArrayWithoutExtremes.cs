848e3eb0d69815d6b05a985549939ddb5b0d463d
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-21 13:52:45 +0200

    #12 Sum numbers without extremes (#14)

---
+++ CodeKata/SumArrayWithoutExtremes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class SumArrayWithoutExtremes
+    {
+        public static int Sum(int[] numbers)
+        {
+            int sum = 0;
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            if (numbers == null || numbers.Length < 2)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var current = numbers[i];
+
+                min = current < min ? current : min;
+                max = current > max ? current : max;
+                sum += current;
+            }
+
+            return sum - (min + max);
+
+        }
+    }
+}
