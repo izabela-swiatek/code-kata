f69274281abab823387f23ffa1717176f7082b31
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-10-24 10:22:22 +0200

    #15 remove exclamation marks

---
+++ CodeKata.UnitTests/RemoveExclamationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class RemoveExclamationTests
+    {
+        [TestCase("", "")]
+        [TestCase("!", "")]
+        [TestCase("!!", "")]
+        [TestCase("Hi!", "Hi")]
+        [TestCase("!?!", "?")]
+        public static void RenoveExclamationMarks_TakeString_ReturnStringWithoutExlamation(string previous, string next)
+        {
+            Assert.That(RemoveExclamation.RemoveExclamationMarks(previous), Is.EqualTo(next));
+           
+        }
+    }
+}
