2a80a3d6dde57c75a3730d80297abc553c0683b9
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-10-24 12:02:27 +0200

    17 return 3rd angle of triangle

---
+++ CodeKata/ThirdTriangleAngle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class ThirdTriangleAngle
+    {
+        public static int OtherAngle(int a, int b) 
+        {
+            return 180 > a + b ? 180 - a - b : 0;
+        }
+    }
+}
