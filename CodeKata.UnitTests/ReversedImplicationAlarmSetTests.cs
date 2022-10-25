6d82beff18c0325a412ef3ecdcc44d918b6659ce
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-25 21:40:42 +0200

    #19 Set alarm when conditions true (#20)

---
+++ CodeKata.UnitTests/ReversedImplicationAlarmSetTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class ReversedImplicationAlarmSetTests
+    {
+        [TestCase(true, true, false)]
+        [TestCase(false, true, false)]
+        [TestCase(false, false, false)]
+        [TestCase(true, false, true)]
+        public void SetAlarm_WhenConditions_ReturnBool(bool firstCondition, bool secondCondition, bool result) 
+        {
+            Assert.That(ReversedImplicationAlarmSet.SetAlarm(firstCondition, secondCondition), Is.EqualTo(result));
+        }
+    }
+}
