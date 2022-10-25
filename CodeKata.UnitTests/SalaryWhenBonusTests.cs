fa1caee7139e6dd12b64de23f9cac47618ffbabc
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-25 21:41:29 +0200

    #2 Return string Salary when bonus (#24)

---
+++ CodeKata.UnitTests/SalaryWhenBonusTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class SalaryWhenBonusTests
+    {
+        [TestCase(10000, true, "$100000")]
+        [TestCase(25000, true, "$250000")]
+        [TestCase(10000, false, "$10000")]
+        [TestCase(60000, false, "$60000")]
+        [TestCase(2, true, "$20")]
+        [TestCase(78, false, "$78")]
+        [TestCase(67890, true, "$678900")]
+        public void BonusTime_GivenSalaryInt_ReturBonusedSalaryString(int intSalary, bool bonus, string stringSalary) 
+        {
+            Assert.That(SalaryWhenBonus.BonusTime(intSalary, bonus), Is.EqualTo(stringSalary));
+        }
+    }
+}
---
+++ CodeKata/SalaryWhenBonus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class SalaryWhenBonus
+    {
+        public static string BonusTime(int salary, bool bonus)
+        {
+            int salaryAfterBonus = bonus ? salary * 10 : salary;
+            return $"${salaryAfterBonus}";
+        }
+    }
+}
