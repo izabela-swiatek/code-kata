9088bd4de6ba055aa5020db800870d622d648157
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-25 21:39:59 +0200

    17 return 3rd angle of triangle (#18)

---
+++ CodeKata.UnitTests/ThirdTriangleAngleTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class ThirdTriangleAngleTests
+    {
+        [TestCase(30, 60, 90)]
+        [TestCase(60, 60, 60)]
+        [TestCase(43, 78, 59)]
+        [TestCase(10, 20, 150)]
+        [TestCase(180, 0, 0)]
+        public void TriangleAngle_TakeTwoAngles_ReturnThird(int a, int b, int c) 
+        {
+            Assert.That(ThirdTriangleAngle.OtherAngle(a, b), Is.EqualTo(c));
+        }
+    }
+}
