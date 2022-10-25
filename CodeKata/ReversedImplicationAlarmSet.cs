45ae50f3cd3b63cf100338c4617d7aaa90eda041
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-10-25 10:13:35 +0200

    #19 Set alarm when conditions true

---
+++ CodeKata/ReversedImplicationAlarmSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class ReversedImplicationAlarmSet
+    {
+        public static bool SetAlarm(bool employed, bool vacation) 
+        {
+            return !(!employed | vacation);
+        }
+    }
+}
