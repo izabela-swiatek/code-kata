43d24dd12995b59a195ec4f4a125e293b958e632
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-25 21:41:56 +0200

    #21 populate an array with numbers one to n (#22)

---
+++ CodeKata.UnitTests/MonkeyCountArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class MonkeyCountArrayTests
+    {
+        [TestCase(5, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(3, new int[] { 1, 2, 3 })]
+        [TestCase(9, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+        [TestCase(10, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+        [TestCase(20, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 })]
+        public void MonkeyCount_GivenArrayLenght_ReturnArray(int arrayLength, int[] counted) 
+        {
+            Assert.That(MonkeyCountArray.MonkeyCount(arrayLength), Is.EqualTo(counted));
+        }
+    }
+}
---
+++ CodeKata/MonkeyCountArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class MonkeyCountArray
+    {
+        public static int[] MonkeyCount(int n)
+        {
+            int[] counted = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                counted[i] = i + 1;
+            }
+
+            return counted;
+        }
+    }
+}
