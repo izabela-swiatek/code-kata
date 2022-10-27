6660da66072039605024e4ecb5feef6b29d59441
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-10-27 10:34:47 +0200

    #15 remove exclamation marks (#16)
    * #15 remove exclamation marks
    
    * #15 modify name of test method

---
+++ CodeKata/RemoveExclamation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class RemoveExclamation
+    {
+        public static string RemoveExclamationMarks(string givenString) 
+        {
+            return givenString.Replace("!", "");
+        }
+    }
+}
