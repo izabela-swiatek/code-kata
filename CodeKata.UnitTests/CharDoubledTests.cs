6700de0e632f40d5c6692a7cded4e0584a69cbf3
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-10-27 11:03:50 +0200

    #27 take string return string with doubled chars

---
+++ CodeKata.UnitTests/CharDoubledTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class CharDoubledTests
+    {
+        [TestCase("abcd", ExpectedResult = "aabbccdd")]
+        [TestCase("Adidas", ExpectedResult = "AAddiiddaass")]
+        [TestCase("1337", ExpectedResult = "11333377")]
+        [TestCase("illuminati", ExpectedResult = "iilllluummiinnaattii")]
+        [TestCase("123456", ExpectedResult = "112233445566")]
+        [TestCase("%^&*(", ExpectedResult = "%%^^&&**((")]
+        public static string DoubleChar_String_ReturnCharDoubledString(string s)
+        {
+            return CharDoubled.DoubleChar(s);
+        }
+    }
+}
