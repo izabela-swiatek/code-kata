df800ca20f3489f48c4196ccab3438ba42953dcd
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-10-27 12:20:28 +0200

    #29 calculate kmPerHour for cmPerSec

---
+++ CodeKata.UnitTests/CockroachSpeedTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class CockroachSpeedTests
+    {
+        [TestCase(1.08, ExpectedResult = 30)]
+        [TestCase(1.09, ExpectedResult = 30)]
+        [TestCase(0, ExpectedResult = 0)]
+        public static int SpeedCmPerH_SpeedKmPerH_ReturnSpeedCmPerSec(double d)
+        {
+            return CockroachSpeed.SpeedCmPerH(d);
+        }
+    }
+}
