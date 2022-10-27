2aeac11cc6ff24de1801cb77e0d44cb0ae295a43
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-10-27 13:22:24 +0200

    #31 compare firsts and lasts chars of strings

---
+++ CodeKata.UnitTests/BeastDishComparisonTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class BeastDishComparisonTests
+    {
+        [TestCase("great blue heron", "garlic naan", ExpectedResult = true)]
+        [TestCase("chickadee", "chocolate cake", ExpectedResult = true)]
+        [TestCase("lion", "lemon pudding", ExpectedResult = false)]
+        public static bool Feast_GivenTwoStrings_ReturnBool(string beast, string dish)
+        {
+            return BeastDishComparison.Feast(beast, dish);
+        }
+    }
+}
---
+++ CodeKata/BeastDishComparison.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class BeastDishComparison
+    {
+        public static bool Feast(string beast, string dish)
+        {
+            return (beast[0] == dish[0] && beast[beast.Length - 1] == dish[dish.Length - 1]);
+        }
+    }
+}
