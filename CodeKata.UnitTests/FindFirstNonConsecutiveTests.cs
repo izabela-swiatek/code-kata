9e91a327e934a828bedb2bf72714ca5ca9265418
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-11-03 13:28:07 +0100

    #33 Find first non-consecutive number

---
+++ CodeKata.UnitTests/FindFirstNonConsecutiveTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class FindFirstNonConsecutiveTests
+    {
+        [TestCase(new int[] { 1, 2, 3, 4, 6, 7, 8 }, ExpectedResult = 6)]
+        [TestCase(new int[] { -8, -7, -6, -5, -2, -1, 0 }, ExpectedResult = -2)]
+        [TestCase(new int[] { -10, -9, -8, -7, -6, -5, -4, -3, -2 }, ExpectedResult = null)]
+        [TestCase(new int[] { -10, -9, -7, -6, -5, -3, -2 }, ExpectedResult = -7)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, ExpectedResult = null)]
+        [TestCase(new int[] { 4 }, ExpectedResult = null)]
+        [TestCase(new int[] { }, ExpectedResult = null)]
+
+        public static object FirstNonConsecutive_GivenArray_ReturnNCNumber(int[] arrTested) 
+        {
+            return FindFirstNonConsecutive.FirstNonConsecutive(arrTested);
+        }
+    }
+}
---
+++ CodeKata/FindFirstNonConsecutive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class FindFirstNonConsecutive
+    {
+        public static object FirstNonConsecutive(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int previousNum = arr[i - 1];
+                int currentNum = arr[i];
+
+                if (currentNum != previousNum + 1)
+                    return currentNum;
+            }
+            return null;
+        }
+    }
+}
