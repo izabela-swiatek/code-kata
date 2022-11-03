651c7f38abd4e79e6de4830791ab89d26f6cd8ae
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-11-03 11:50:53 +0100

    #27 take string return string with doubled chars (#28)

---
+++ CodeKata/CharDoubled.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class CharDoubled
+    {
+        public static string DoubleChar(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    sb.Append(s[i]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
