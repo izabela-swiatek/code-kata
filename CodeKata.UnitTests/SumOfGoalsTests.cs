f62d46fa9e9c4fb2e79cb898cb60885f9945df1b
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-11-03 11:51:15 +0100

    #25 Sum the number of Goals (#26)

---
+++ CodeKata.UnitTests/SumOfGoalsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class SumOfGoalsTests
+    {
+        [TestCase(0, 0, 0, 0)]
+        [TestCase(42, 4, 12, 58)]
+        [TestCase(0, 15, 0, 15)]
+        [TestCase(1, 1, 1, 3)]
+        public void GetGoals_Given3Numbers_ReturnSum(int laLiga, int copa, int champions, int sum) 
+        {
+            Assert.That(SumOfGoals.GetGoals(laLiga, copa, champions), Is.EqualTo(sum));
+        }
+    }
+}
