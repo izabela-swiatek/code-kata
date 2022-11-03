7379e2191e2e6a962e6e40402e88f7e6c4fd6268
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-11-03 11:50:03 +0100

    #29 calculate kmPerHour for cmPerSec (#30)

---
+++ CodeKata/CockroachSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class CockroachSpeed
+    {
+        public static int SpeedCmPerH(double x)
+        {
+            double cmPerSec = 100000d / 3600d;
+            return (int)(x * cmPerSec);
+        }
+    }
+}
