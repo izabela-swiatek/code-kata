e8b3e5c655901ac7a6cb36b371760804583bb6a5
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-11-04 13:56:04 +0100

    #35 Return doubledAge difference

---
+++ CodeKata/TwiceAsOldSolution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class TwiceAsOldSolution
+    {
+        public static int TwiceAsOld(int dadYears, int sonYears)
+        {
+            return dadYears - 2 * sonYears > 0 ? dadYears - 2 * sonYears : (dadYears - 2 * sonYears) * (-1);
+        }
+    }
+}
