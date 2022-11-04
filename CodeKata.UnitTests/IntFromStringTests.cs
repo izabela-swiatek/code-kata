91db9fe6ed7e00664df8f3f3a51e482eb7d676a7
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-11-04 15:05:21 +0100

    #37 Parse int from String sequence

---
+++ CodeKata.UnitTests/IntFromStringTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class IntFromStringTests
+    {
+        [TestCase("3 years old", ExpectedResult = 3)]
+        [TestCase("4 years old", ExpectedResult = 4)]
+        [TestCase("9 years old", ExpectedResult = 9)]
+        public static int GetAge_GivenString_ReturnInt(string stringAge) 
+        {
+            return IntFromString.GetAge(stringAge);
+        }
+
+    }
+}
---
+++ CodeKata/IntFromString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class IntFromString
+    {
+        public static int GetAge(string inputString)
+        {
+            return int.Parse(inputString.Substring(0, 1));
+        }
+    }
+}
