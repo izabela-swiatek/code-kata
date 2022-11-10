9706960ed9594f3b3213e95d6c2cde02e56b9a2d
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-11-10 09:27:45 +0100

    #37 Parse int from String sequence (#38)
    * #37 Parse int from String sequence
    
    * Modify test function name

---
+++ CodeKata/IntFromString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class IntFromString
+    {
+        public static int GetAge(string inputString)
+        {
+            return int.Parse(inputString.Substring(0, 1));
+        }
+
+    }
+}
