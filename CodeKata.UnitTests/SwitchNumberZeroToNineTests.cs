7a47ce5e57d3bf42b05b63de34757089e05f9606
izabela-swiatek <109695705+izabela-swiatek@users.noreply.github.com>
2022-11-16 13:51:32 +0100

    #39 switch number zero to nine (#40)

---
+++ CodeKata.UnitTests/SwitchNumberZeroToNineTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class SwitchNumberZeroToNineTests
+    {
+        [TestCase(1, ExpectedResult = "One")]
+        [TestCase(3, ExpectedResult = "Three")]
+        [TestCase(5, ExpectedResult = "Five")]
+        [TestCase(0, ExpectedResult = "Zero")]
+        [TestCase(10, ExpectedResult = "Invalid number")]
+        public static string SwitchItUp_GivenNumber_ReturnText(int number)
+        {
+            return SwitchNumberZeroToNine.SwitchItUp(number);
+        }
+    }
+
+}
---
+++ CodeKata/SwitchNumberZeroToNine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class SwitchNumberZeroToNine
+    {
+        public static string SwitchItUp(int number)
+        {
+            var result = number switch
+            {
+                0 => "Zero",
+                1 => "One",
+                2 => "Two",
+                3 => "Three",
+                4 => "Four",
+                5 => "Five",
+                6 => "Six",
+                7 => "Seven",
+                8 => "Eight",
+                9 => "Nine",
+                _ => "Invalid number"
+            };
+
+            return result;
+        }
+
+    }
+}
