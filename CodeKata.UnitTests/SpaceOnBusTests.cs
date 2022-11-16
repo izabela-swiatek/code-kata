42d7d7f707db12954b6c54b614c976d7e8a69421
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-11-16 14:53:30 +0100

    #41 Calculate number of passenegers above bus capacity

---
+++ CodeKata.UnitTests/SpaceOnBusTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class SpaceOnBusTests
+    {
+        [TestCase(10, 5, 5, ExpectedResult = 0)]
+        [TestCase(100, 60, 50, ExpectedResult = 10)]
+        [TestCase(20, 5, 5, ExpectedResult = 0)]
+        public static int Enough_GivenSpaceCount_ReturnLeftPassengersCount(int seats, int passengersIn, int passengersOut) 
+        {
+            return SpaceOnBus.Enough(seats, passengersIn, passengersOut);
+        }
+
+    }
+}
---
+++ CodeKata/SpaceOnBus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class SpaceOnBus
+    {
+        public static int Enough(int cap, int on, int wait)
+        {
+            int isEnough = cap - on - wait;
+            return isEnough > 0 ? 0 : Math.Abs(isEnough);
+        }
+    }
+}
