1bee57baea2feffcad5d65f3b81c37f9d8fea665
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-11-17 09:53:24 +0100

    #43 Count vowels in given text

---
+++ CodeKata/VowelCounting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class VowelCounting
+    {
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            vowelCount = str.Count(v => vowels.Contains(v));
+
+            return vowelCount;
+        }
+    }
+}
