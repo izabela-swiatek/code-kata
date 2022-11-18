47043bdfd67fb6b88db39b3b9734b06abb8d44ca
Izabela Swiatek <izabela.k.swiatek@gmail.com>
2022-11-18 09:36:46 +0100

    #45 Remove vowels from given text

---
+++ CodeKata.UnitTests/VowelsRemovingTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.UnitTests
+{
+    internal class VowelsRemovingTests
+    {
+        [TestCase("This website is for losers LOL!", ExpectedResult = "Ths wbst s fr lsrs LL!")]
+        [TestCase("No offense but,\nYour writing is among the worst I've ever read", ExpectedResult = "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd")]
+        [TestCase("What are you, a communist?", ExpectedResult = "Wht r y,  cmmnst?")]
+        public static string Disemvowel_TakeText_ReturnTextWithoutVowels(string vowelString) 
+        {
+            return VowelsRemoving.Disemvowel(vowelString); 
+        }
+
+    }
+}
---
+++ CodeKata/VowelsRemoving.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CodeKata
+{
+    public class VowelsRemoving
+    {
+        public static string Disemvowel(string str)
+        {
+            Regex regex = new Regex(@"[aeiouAEIOU]");
+            string noVoweledString = regex.Replace(str, "");
+            return noVoweledString;
+        }
+    }
+}
